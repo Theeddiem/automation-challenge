0cdeec8f2e8b66cfb869710ee7abcccb515f0aa8
theeddiem <eddieknaz@gmail.com>
2020-10-01 16:37:26 +0300

    improveing try and ctach

--- backend/service/ItemService.js
+++ backend/service/ItemService.js
@@ -8,95 +8,62 @@ module.exports = class ItemService {
 
     async getAllItems()
     {
-     
         return Inventory.getAll();
     }
 
     async getItemById (id)
     {
-        // try {
-            const item = Inventory.get(id)
-            if(!item)
-                throw new Error("The item with the given ID was not found")
+        const item = Inventory.get(id)
+        if(!item)
+            throw new Error("The item with the given ID was not found")
 
-            return item;
-        // } catch (error) {
-        //     console.log("s");
-        //     throw error
-        // }
+         return item;
     }
 
     async updateItem(item)
     {
-        try {
-            const {id, name, description, count} = item;
-            const updatedItem = await this.getItemById(id)
+        const {id, name, description, count} = item;
+        const updatedItem = await this.getItemById(id)
 
-            await validateItem(item)   
-            updatedItem.name = name;
-            updatedItem.description = description;
-            updatedItem.count = count;
-            return updatedItem;
-
-        } catch (error) {
-           throw error
-        }
+        await validateItem(item)   
+        updatedItem.name = name;
+        updatedItem.description = description;
+        updatedItem.count = count;
+        return updatedItem;   
     }
 
     async addItem(item)
     {
-        try {
-            await validateItem(item)
-            const {name, description, count} = item
-            const newItem = new Item(name, description, count);
-            return Inventory.set(newItem)
-
-        } catch (error) {
-            throw error;
-        }                  
+      
+        await validateItem(item)
+        const {name, description, count} = item
+        const newItem = new Item(name, description, count);
+        return Inventory.set(newItem)                   
     }
 
     async removeItemById(id)
     {
-        // try {
-            if(!Inventory.delete(id))
-                throw new Error("The item with the given ID was not found")
+        if(!Inventory.delete(id))
+            throw new Error("The item with the given ID was not found")
             
-            return "The item with the given ID was successfully deleted";
-
-
-        // } catch (error) {
-        //     throw error
-        // }       
+        return "The item with the given ID was successfully deleted";  
     }
 
     async withdrawItem(payload)
     {
         
         const {id, amount} = payload
-        try {
-            const item = await this.getItemById(id)
-            item.withdraw(amount)
-            return item;
-
-        } catch (error) {
-            throw error
-        }
-        
+        const item = await this.getItemById(id)
+        item.withdraw(amount)
+         return item;       
     }
 
     async depositItem(payload)
     {
         const {id, amount} = payload
-        try {
-            const item = await this.getItemById(id)
-            item.deposit(amount)
-            return item;
-
-        } catch (error) {
-            throw error
-        }
-      
+        const item = await this.getItemById(id)
+        item.deposit(amount)
+        return item;    
     }
 }
 
